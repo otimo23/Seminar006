3c46fab7c2c7da49b7ec1995d010f501a0a53316
olga Timoshenko <otimoshenko55@gmail.com>
2023-03-02 15:15:24 +0100

    перевернёт одномерный массив

--- Seminar006_1/Program.cs
+++ Seminar006_1/Program.cs
@@ -1,30 +1,46 @@
 ﻿/*Задача 39:** Напишите программу,
  которая перевернёт одномерный массив 
- (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+ (последний элемент будет на первом месте, а первый - на последнем и т.д.) Два способа
 [1 2 3 4 5] -> [5 4 3 2 1]
 [6 7 3 6] -> [6 3 7 6]*/
-int[] startarray = GetRandomArray(7, 1, 10);
-Console.WriteLine(string.Join(", ", startarray));
-startarray = ReversMass (startarray);
-Console.Write  (startarray);
+Console.Clear();
 
-int[] GetRandomArray(int size, int minValue, int maxValue)
+int[] array = GetArray(10, 0, 10);
+Console.WriteLine(String.Join(" ", array));
+
+int[] reversArray = ReversArray2(array);
+Console.WriteLine(String.Join(" ", reversArray));
+
+/*ReversArray1(array);
+Console.WriteLine(String.Join(" ", array));*/
+
+int[] GetArray(int size, int minValue, int maxValue)
 {
-    int[] result = new int[size];
+    int[] res = new int[size];
+
     for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(minValue, maxValue + 1);
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+/*void ReversArray1(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length/2; i++)
+    {
+        int k = inArray[i];// первое значение
+        inArray[i] = inArray[inArray.Length - i - 1];  //последнее значение ложим в первое
+        inArray[inArray.Length - i - 1] = k;//первое значение ложим в последний ИНДЕКС
+    }
+}*/
+
+int[] ReversArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+
     }
     return result;
-}
-int [] ReversMass (int[] array)
-{
-int[]ar = new int[array.Length];
-int j = 0;
-for(int i = array.Length; i >=0; i--)
-{
-    ar[j] = array[i];
-    j++;
-}
-return ar;
 }
