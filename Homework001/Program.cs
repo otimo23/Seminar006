e37caf169fbd89f26f71e1b12015222c9dc4ae4e
olga Timoshenko <otimoshenko55@gmail.com>
2023-03-02 15:20:03 +0100

    Пользователь вводит с клавиатуры M чисел. Посчитайте,сколько чисел больше 0 ввёл пользователь

---
+++ Homework001/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2*/
+
+Console.WriteLine("Enter numbers: ");
+string elem = Console.ReadLine();
+int[] basearray = GetStrArray(elem);
+Console.WriteLine($"Positive numbers - {GetCount(basearray)}");
+
+int[] GetStrArray(string StringAr)
+{
+    string[] ar = StringAr.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[ar.Length];
+    for (int i = 0; i < ar.Length; i++)
+    {
+        result[i] = int.Parse(ar[i]);
+    }
+    return result;
+}
+
+int GetCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0)
+            count = count + 1;
+    }
+    return count;
+}
