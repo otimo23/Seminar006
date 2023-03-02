616202f140a842ae81ec5bdf156da6110474ecf9
olga Timoshenko <otimoshenko55@gmail.com>
2023-03-02 15:18:55 +0100

    апишите программу,которая будет преобразовывать десятичное число в двоичное

---
+++ Seminar006_3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, 
+//которая будет преобразовывать десятичное число в двоичное.
+
+Console.Clear();
+int number = int.Parse(Console.ReadLine());
+
+string res = DecToNum(number, 2);
+
+Console.WriteLine($"{number}->{res}");
+
+
+//Универсальный математический для перевода из 10 в любую
+string DecToNum(int decNumber, int otherSystem)
+{
+    string res = "";
+    string nums = "0123456789ABCDEF";
+    while (decNumber > 0)
+    {
+        int ost = decNumber / otherSystem;
+        res = nums[decNumber - otherSystem * ost] + res;
+        decNumber /= otherSystem;
+    }
+    return res;
+}
